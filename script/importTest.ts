177ca67d6332456affa4a22f2347676e68322aca
DaoNz <ionmax5602@naver.com>
2024-03-29 00:29:13 +0900

    feat : Add ts test script

---
+++ script/importTest.ts
@@ -0,0 +1,16 @@
+
+class ImportTest {
+	
+	constructor() {
+		console.log("Create Constructor")
+	}
+	
+	consoleTestStr = (str : String) => {
+		console.log("Console Test Str --", str)
+	}
+	
+	consoleTestNum = (num : number) => {
+		console.log("Console Test Num --", num)
+	}
+	
+}
