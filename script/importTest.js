fbb19141557be15bb78f6822402f0aa74435f9ec
DaoNz <ionmax5602@naver.com>
2024-03-29 00:29:13 +0900

    feat : Add ts test script

--- script/importTest.js
+++ script/importTest.js
@@ -1,12 +1,12 @@
-
-class ImportTest {
-	
-	constructor() {
-		console.log("Create Constructor")
-	}
-	
-	consoleTest = (str) => {
-		console.log("Console Test --", str)
-	}
-	
-}
+var ImportTest = /** @class */ (function () {
+    function ImportTest() {
+        this.consoleTestStr = function (str) {
+            console.log("Console Test Str --", str);
+        };
+        this.consoleTestNum = function (num) {
+            console.log("Console Test Num --", num);
+        };
+        console.log("Create Constructor");
+    }
+    return ImportTest;
+}());
